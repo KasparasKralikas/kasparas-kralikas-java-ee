0ad2577694012a38cabd09ae978d91d0f2f392d1
KasparasKralikas <kasparaskr@unity3d.com>
2020-04-20 16:29:44 +0300

    Fixed the issues with myBatis generation, introduced missing files

---
+++ src/main/java/lt/mif/vu/usecases/ProjectsMyBatis.java
@@ -0,0 +1,40 @@
+package lt.mif.vu.usecases;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import lt.mif.vu.mybatis.dao.ProjectMapper;
+import lt.mif.vu.mybatis.model.Project;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Model
+public class ProjectsMyBatis {
+    @Inject
+    private ProjectMapper projectMapper;
+
+    @Getter
+    private List<Project> allProjects;
+
+    @Getter @Setter
+    private Project projectToCreate = new Project();
+
+    @PostConstruct
+    public void init() {
+        this.loadAllProjects();
+    }
+
+    private void loadAllProjects() {
+        this.allProjects = projectMapper.selectAll();
+    }
+
+    @Transactional
+    public String createTeam() {
+        projectMapper.insert(projectToCreate);
+        return "/myBatis/projects?faces-redirect=true";
+    }
+}
